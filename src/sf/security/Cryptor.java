a140c008b02d3dd25c1aa70d59f30ba4c8bad104
kang hwan ki <xangfi@gmail.com>
2013-04-23 10:59:49 +0900

    데이터 암호화 추가

---
+++ src/sf/security/Cryptor.java
@@ -0,0 +1,34 @@
+package sf.security;
+
+/**
+ * 데이터 암호화
+ * 
+ * @author kangki
+ * @since 2013. 4. 23.
+ * @version 1.0
+ * @see Cryptor
+ * @history
+ * 
+ * <pre>
+ */
+public interface Cryptor {
+	/**
+	 * 암호화
+	 * @author kangki
+	 * @see Cryptor.encrypt
+	 * @param src 데이터
+	 * @return byte[] 암호화한 데이터
+	 * @throws Exception 
+	 */
+	byte[] encrypt(byte[] src) throws Exception;
+	
+	/**
+	 * 복호화
+	 * @author kangki
+	 * @see Cryptor.decrypt
+	 * @param src 암호화된 데이터
+	 * @return byte[] 복호화한 데이터
+	 * @throws Exception 
+	 */
+	byte[] decrypt(byte[] src) throws Exception;
+}
